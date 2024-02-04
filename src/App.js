710e41211f89b739797be1c2fb5d9042d9b5347c
Jakkapan Pakeerat <jakkapanpakeerat@Jakkapans-MacBook-Air.local>
2024-02-04 12:51:16 +0700

    add code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
+const phrases = [
+  "Decline",
+  "Are you sure",
+  "Really sure",
+  "Don't do this to me",
+  "I'm gonna cry..",
+  "You're breaking my heart ;(",
+];
+
+const images = [
+  "https://media.tenor.com/J6xumGwaxh8AAAAj/flowers-flower.gif",
+  // "https://media.tenor.com/Mg-5tsOZZSMAAAAj/peach-goma-stuffed-pillow.gif",
+  "https://media.tenor.com/0Hw-vPVORlEAAAAj/peach-goma-phone-lying-down.gif",
+  "https://media.tenor.com/ti6jDf8qC5IAAAAj/peach-and-goma-goma.gif",
+  // "https://media.tenor.com/RS6g_7a2eF4AAAAj/peach-and-goma-goma.gif",
+  "https://media.tenor.com/JulL3dye-BoAAAAj/peach-goma-peach-and-goma.gif",
+  // "https://media.tenor.com/XwnmBt4nEigAAAAM/peach-goma-sorry.gif",
+  "https://media.tenor.com/wqYFElBnfoYAAAAj/yelynn-yelynnn.gif",
+
+  "https://media.tenor.com/Uac8Y5yud28AAAAj/peach-and-goma-goma.gif",
+];
+
+const happy = "https://media.tenor.com/z6X7ejA5Vs0AAAAj/peach-goma-love.gif";
 function App() {
+  const [declineCount, setDeclineCount] = React.useState(0);
+  const [resValentine, setResValentine] = React.useState(false);
+
+  const yesButtonSize = declineCount * 14 + 16;
+
+  const nextText = () => phrases[Math.min(declineCount, phrases.length - 1)];
+  const nextImage = () => images[Math.min(declineCount, images.length - 1)];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{ fontFamily: "Kanit, sans-serif", fontSize: 57 }}>
+          {!resValentine ? " Will you be my valentine?" : "Happy valentine :)"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!resValentine ? (
+          <img src={nextImage()} alt="reaction" />
+        ) : (
+          <img src={happy} alt="reaction" />
+        )}
+
+        {!resValentine ? (
+          <div className="button-container">
+            <button
+              className="yes-button"
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setResValentine(!resValentine)}
+            >
+              Yes
+            </button>
+
+            <button
+              className="decline-button"
+              style={{ fontSize: 16 }}
+              onClick={() => setDeclineCount(declineCount + 1)}
+            >
+              {nextText()}
+            </button>
+          </div>
+        ) : null}
       </header>
     </div>
   );
